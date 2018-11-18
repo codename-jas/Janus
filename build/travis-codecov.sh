6ab9d6361bfd2c5806a84f2b270496c4fcd95872
Pradeep Singh <contact@pradeep.vip>
2018-11-19 04:43:31 +0700

    added code coverage and build scripts

--- build/travis-codecov.sh
+++ build/travis-codecov.sh
@@ -1,5 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 FILE_NAME="$DIR/../codecov.xml"
 curl -s https://codecov.io/bash > codecov
-chmod +x codecov
 ./codecov -f "$FILE_NAME" -t "12073bb6-dd4d-4a10-9731-6b7eaa7bf559"
